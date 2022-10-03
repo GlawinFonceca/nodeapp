cab3daa18587a6c208cc014cfb57b2273e19d049
GlawinFonceca <glawin.f@juegostudioz.com>
2022-10-03 14:47:54 +0530

    commit

---
+++ yargs.js
@@ -0,0 +1,61 @@
+const yargs = require('yargs');
+
+
+// console.log(process.argv);
+// yargs.version('1.1.0')
+
+//create add command
+
+yargs.command({
+    command:'add',
+    describe: 'adding a new note',
+    builder :{
+        title:{
+            describe: 'note title',
+            demandOption: true,
+            type :'string'
+        },
+        body:{
+            describe: "body name",
+            demandOption:true,
+            type: 'string'
+        }
+
+    },
+    handler: function(argv){
+        console.log('title:' +argv.title);
+        console.log('body name:'+ argv.body);
+    }
+})
+
+//create remove command
+
+yargs.command({
+    command : 'remove',
+    describe : 'removing a note',
+    handler :function(){
+        console.log('removing the note');
+    }
+})
+
+//create list command
+
+yargs.command({
+    command : 'list',
+    describe : 'list your  note',
+    handler :function(){
+        console.log('listing out all notes');
+    }
+})
+
+//create read command
+
+yargs.command({
+    command : 'read',
+    describe : 'reading a note',
+    handler :function(){
+        console.log('reading the note');
+    }
+})
+yargs.parse();
+
